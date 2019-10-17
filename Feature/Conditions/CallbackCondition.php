ca52eb179342c90fd41d7841dff8348a108fd375
Daniel Barty <nixspam@web.de>
2019-10-17 20:14:20 +0200

    Implement CallbackCondition and test

---
+++ Feature/Conditions/CallbackCondition.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace E7\FeatureFlagsBundle\Feature\Conditions;
+
+use E7\FeatureFlagsBundle\Context\ContextInterface;
+use InvalidArgumentException;
+
+/**
+ * Class CallbackCondition
+ * @package E7\FeatureFlagsBundle\Feature\Conditions
+ */
+class CallbackCondition extends AbstractCondition
+{
+    /** @var callable */
+    private $callback;
+    
+    /**
+     * Constructor
+     * 
+     * @param callable $callback
+     */
+    public function __construct($callback)
+    {
+        if (!is_callable($callback)) {
+            throw new InvalidArgumentException('First parameter must be callable.');
+        }
+        
+        $this->callback = $callback;
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function getName(): string 
+    {
+        return 'callback';
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    protected function doVote(ContextInterface $context)
+    {
+        return call_user_func($this->callback, $context);
+    }
+}
---
+++ Tests/Feature/Conditions/CallbackConditionTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace E7\FeatureFlagsBundle\Tests\Feature\Conditions;
+
+use E7\FeatureFlagsBundle\Context\Context;
+use E7\FeatureFlagsBundle\Feature\Conditions\CallbackCondition;
+use InvalidArgumentException;
+
+/**
+ * Class CallbackConditionTest
+ * @package E7\FeatureFlagsBundle\Tests\Feature\Conditions
+ */
+class CallbackConditionTest extends ConditionTestCase
+{
+    public function testToStringConversion()
+    {
+        $this->doTestToStringConversion(new CallbackCondition(function() {}));
+    }
+    
+    /**
+     * @dataProvider providerConstructorWithException
+     * @param array $input
+     * @param array $expected
+     */
+    public function testConstructorWithException(array $input, array $expected)
+    {
+        if (null !== $expected['exception']) {
+            $this->expectException($expected['exception']);
+        }
+        
+        $this->assertInstanceOf(
+            CallbackCondition::class, 
+            new CallbackCondition($input['callback'])
+        );
+    }
+    
+    /**
+     * @return array
+     */
+    public function providerConstructorWithException()
+    {
+        return [
+            'valid-callback' => [
+                [ 'callback' => function() { return true; } ],
+                [ 'exception' => null ]
+            ],
+            'invalid-callback-throws-exception' => [
+                [ 'callback' => 'this-is-not-a-valid-callback' ],
+                [ 'exception' => InvalidArgumentException::class ]
+            ],
+        ];
+    }
+    
+    /**
+     * @dataProvider providerVote
+     * @param array $input
+     * @param array $expected
+     */
+    public function testVote(array $input, array $expected)
+    {
+        $condition = new CallbackCondition($input['callback']);
+        
+        $this->assertEquals($expected['result'], $condition->vote(new Context()));
+    }
+    
+    /**
+     * @return array
+     */
+    public function providerVote()
+    {
+        return [
+            'valid-callback-says-yes' => [
+                [ 'callback' => function() { return true; } ],
+                [ 'result' => true,
+                    'exception' => null ]
+            ],
+            'valid-callback-says-no' => [
+                [ 'callback' => function() { return false; } ],
+                [ 'result' => false ]
+            ]
+        ];
+    }
+}
