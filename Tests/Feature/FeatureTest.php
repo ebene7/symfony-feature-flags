a1f5d88168bc6b2b687d48bd90c56697568d23cd
Daniel Barty <nixspam@web.de>
2019-10-24 16:23:33 +0200

    Draft stream wrapper for testing

--- Tests/Feature/FeatureTest.php
+++ Tests/Feature/FeatureTest.php
@@ -117,19 +117,69 @@ class FeatureTest extends TestCase
         ];
     }
 
-//    public function testIsEnabledPassesFeatureIntoContext()
-//    {
-//        // prepare
-//        $feature = new Feature('awesome-feature');
-//        $context = $this->getMockBuilder(Context::class)
-//            ->setMethods(['set', 'remove'])
-//            ->getMock();
-//
-//        $context->expects($this->any())->method('set')->with($this->$this->callback(function () { print_r(func_num_args()); return true; }));
-//
-//
-//        $context->expects($this->any())->method('remove')->with($this->callback(function () { print_r(func_get_args()); return true; }));
-//
-//        $this->assertInternalType('bool', $feature->isEnabled($context));
-//    }
+    public function testIsEnabledPassesFeatureIntoContext()
+    {
+        // prepare
+        $feature = new Feature('awesome-feature');
+        $context = $this->getMockBuilder(Context::class)
+            ->setMethods(['set', 'remove'])
+            ->getMock();
+
+        $c = new Collector();
+    $x = 41;
+        $cb = function () use ($c, &$x) { $x = 42; /* file_put_contents(sys_get_temp_dir() . '/a2', json_encode(func_get_args()), FILE_APPEND);*/ };
+
+        $context->expects($this->any())->method('set')
+//            ->with($this->callback(function () { print_r(func_get_args()); return true; }))
+            ->will($this->returnCallback(function() use (&$x) { $x++; }));
+
+
+//        $context->expects($this->any())->method('remove')
+////            ->with($this->callback(function () { print_r(func_get_args()); return true; }))
+//            ->will($this->returnCallback($cb));
+
+        echo "DATA $x #";
+        $this->assertInternalType('bool', $feature->isEnabled($context));
+    }
 }
+
+class Collector {
+    private $data = [];
+    public function __invoke() {  /*file_put_contents(sys_get_temp_dir() . '/a', json_encode(func_get_args()), FILE_APPEND); echo 'HIER';*/ }
+    public function add() { /*file_put_contents(sys_get_temp_dir() . '/a', json_encode(func_get_args()), FILE_APPEND); */}
+    public function getData() {/* return '##'.file_get_contents(sys_get_temp_dir() . '/a');*/ }
+}
+//
+// https://www.php.net/manual/en/class.streamwrapper.php
+//
+//class ArrayStreamWrapper
+//{
+//    private $data = [];
+//    public $context;
+//
+//    public function __construct () {}
+//    public function __destruct () {}
+//    public function dir_closedir () : bool {}
+//    public function dir_opendir (string $path, int $options) : bool {}
+//    public function dir_readdir () : string {}
+//    public function dir_rewinddir () : bool {}
+//    public function mkdir (string $path, int $mode, int $options) : bool {}
+//    public function rename (string $path_from , string $path_to) : bool {}
+//    public function rmdir (string $path , int $options) : bool {}
+//    public function stream_cast (int $cast_as) : resource {}
+//    public function stream_close () {}
+//    public function stream_eof () : bool {}
+//    public function stream_flush () : bool {}
+//    public function stream_lock (int $operation ) : bool {}
+//    public function stream_metadata (string $path , int $option , mixed $value ) : bool {}
+//    public function stream_open (string $path , string $mode , int $options , string &$opened_path ) : bool {}
+//    public function stream_read (int $count ) : string {}
+//    public function stream_seek (int $offset , int $whence = SEEK_SET ) : bool {}
+//    public function stream_set_option (int $option , int $arg1 , int $arg2 ) : bool {}
+//    public function stream_stat () : array {}
+//    public function stream_tell () : int {}
+//    public function stream_truncate (int $new_size) : bool {}
+//    public function stream_write (string $data) : int {}
+//    public function unlink (string $path) : bool {}
+//    public function url_stat (string $path , int $flags) : array {}
+//}
